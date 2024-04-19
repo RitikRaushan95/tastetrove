e6167260d489b684e66076c61e917491fb8f7735
Ritik <ritikraushan160@gmail.com>
2024-04-20 00:58:52 +0530

    commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,12 +5,12 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
-  // const [login,setLogin]=useState("Login")
+  const [login,setLogin]=useState("Login")
 
-  // const change=()=>{
-  //   // login==="Login"?setLogin("Logout"):setLogin("Login");
-  //   console.log(login)
-  // }
+  const change=()=>{
+     login==="Login"?setLogin("Logout"):setLogin("Login");
+    console.log(login)
+  }
   return (
     <>
     <header className="text-gray-600 body-font cursor-pointer ">
@@ -24,7 +24,7 @@ function Navbar() {
       <Link to='/about' className="mr-5 hover:text-gray-900 hover:text-orange-500">About</Link>
       <Link to='/contact' className="mr-5 hover:text-gray-900 hover:text-orange-500">Contact</Link>
     </nav>
-    <button className="flex justify-center items-center bg-gray-300 border-0 py-1 px-3  focus:outline-none hover:bg-orange-500 rounded text-base mt-4 md:mt-0 text-black">Login
+    <button className="flex justify-center items-center bg-gray-300 border-0 py-1 px-3 w-20  focus:outline-none hover:bg-orange-500 rounded text-base mt-4 md:mt-0 text-black" onClick={change}>{login}
     </button>
   </div>
 </header>
